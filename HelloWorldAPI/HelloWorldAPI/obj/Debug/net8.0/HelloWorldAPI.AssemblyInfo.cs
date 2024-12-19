f9df7a31f4615bd3a7571ea2679851c0c1bf04ed
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2024-12-19 11:38:11 +0700

    BTH1

--- HelloWorldAPI/HelloWorldAPI/obj/Debug/net8.0/HelloWorldAPI.AssemblyInfo.cs
+++ HelloWorldAPI/HelloWorldAPI/obj/Debug/net8.0/HelloWorldAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5becbc8e01c30bb1e1c3badcd5d1bad9119cd9ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b94225d1f5e0bc80f2084426cdf79ed36be9f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
